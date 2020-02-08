7e3522cd7ebb0b7036514d9762469a6f6c0babdc
elsareutersward <elsa.reutersward@gmail.com>
2020-02-08 20:09:24 +0100

    Version 1

---
+++ script.js
@@ -0,0 +1,68 @@
+//Constants
+const vegetarian = "Vegetarian Pizza"
+const hawaiian = "Hawaiian Pizza"
+const pepperoni = "Pepperoni Pizza"
+
+const pizzaPrice = 80
+
+var pizzas = document.getElementsByClassName("pizza");
+var selectedPizza;
+
+
+//Functions
+function toggle() {
+    if (selectedPizza) {
+        selectedPizza.classList.toggle("active");
+    }
+    this.classList.toggle("active");
+    selectedPizza = this;
+}
+
+function getOrderName() {
+    if (selectedPizza) {
+        return selectedPizza.id;
+    } else {
+        return
+    }
+}
+
+function getInputValue() {
+    return document.getElementById("pizza-quantity").value;
+}
+
+const totalCost = (orderQuantity) => {
+ return orderQuantity * pizzaPrice
+}
+
+const cookingTime = (orderQuantity) => {
+ if (orderQuantity <= 2) {
+    return 10;
+ } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+    return 15;
+ } else if (orderQuantity > 5) {
+    return 20;
+ } else {
+    return 0;
+ }
+}
+
+
+//Stuff that do stuff
+for (var i = 0; i < pizzas.length; i++) {
+ pizzas[i].onclick = toggle
+}
+
+function orderPizza() {
+    const pizzaName = getOrderName();
+    const orderQuantity = getInputValue();
+    if (orderQuantity && orderQuantity > 0 && pizzaName) {
+       document.getElementById("final-greeting").innerHTML = 
+        `Great, I'll get started on your ${pizzaName} right away, it will cost ${totalCost(orderQuantity)} kr. 
+        The pizza will take ${cookingTime(orderQuantity)} minutes.`   
+    } else {
+      document.getElementById("final-greeting").innerHTML = `Please select a pizza from the menu and your desired quantity`  
+    }
+  }
+
+
+  
